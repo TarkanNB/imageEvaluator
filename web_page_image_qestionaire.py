82c6cba7172ee77e0c41513f1958a4ee24688171
TarkanNB <129499101+TarkanNB@users.noreply.github.com>
2023-04-11 18:49:09 +0200

    Database storage and bug fix of configuration file
    Fixed the bugs when reading the configuration file.
    All questions in configuration file will now be rendered in on the webpage and responses will be stored in a datasheet.

--- web_page_image_qestionaire.py
+++ web_page_image_qestionaire.py
@@ -10,27 +10,45 @@ import csv
 import random as rd
 from PIL import Image
 import streamlit as st
-
+import streamlit.components.v1 as components
 
 
 class Picture:
     def __init__(self, sample_name, types, extension):
         self.sample = sample_name   # string
         self.types = types   # list of str
-        self.full_name = dict()
+        self.full_name_of = dict()
+        self.full_names = []
         for t in types:
-            self.full_name[t] = sample_name + "__" + t + "." + extension
-
-    def all_variants(self):
-        self.full_name.values()  # list of str
+            full_name = sample_name + "__" + t + "." + extension
+            self.full_names.append(full_name)
+            self.full_name_of[t] = full_name
+        self.full_names = tuple(self.full_names)
 
     def true_size(self):
-        img = Image.open(self.full_name.values()[0])
+        img = Image.open(self.full_names[0])
         return img.size
+
+    def standard_show(self, the_type, size_str):
+        path_to_image = "images/" + self.full_name_of[the_type]
+        img = Image.open(path_to_image)
+        width, height = img.size
+        size = size_str.strip(" ").strip("(").strip(")").strip(" ").split(",")
+        if not size_str:
+            st.image(img, output_format='png')
+            return None
+        elif len(size) == 1:
+            image_size = (int(size[0]), int(size[0]))
+        elif len(size) == 2:
+            image_size = (int(size[0]), int(size[1]))
+        else:
+            raise Exception("Error: Could not interpret the Default scale in configuration file.")
+        new_img = img.resize(image_size)
+        st.image(new_img, output_format='png')
 
     def show(self, the_type, scale=1):
         # rendes the image on the webpage with specified scale.
-        path_to_image = "images/" + self.full_name[the_type]
+        path_to_image = "images/" + self.full_name_of[the_type]
         img = Image.open(path_to_image)
         if scale == 1:
             st.image(img, output_format='png')
@@ -41,8 +59,8 @@ class Picture:
             st.image(new_img, output_format='png')
 
 class Question:
-    def __init__(self, key, database_name, type_of_question, question_discription, possible_answers=None):
-        self.database_name = database_name
+    def __init__(self, key, name_in_database, type_of_question, question_discription, possible_answers=None):
+        self.name_in_database = name_in_database
         self.type = type_of_question
         self.discription = question_discription
         self.options = possible_answers
@@ -115,7 +133,7 @@ def get_all_picture(type_selection=None):
 def get_configurations():
     # [0]: fetches all conciguration veriables from the configuration.json file and stores it in a dictionary,
     # [1]: fetches all questions in configuration.json file and stores and stores them in a list of Questions(class).
-    configurations = dict()
+    configuration = dict()
     questions = []
     question_variables = []
     key = 0  # unic key for each question.
@@ -139,7 +157,7 @@ def get_configurations():
                     reading_input = True  # start reading other variable from the file.
             elif reading_input:
                 # bind value to declared variable
-                configurations[variable] = line.split("$")[1]
+                configuration[variable] = line.split("$")[1]
                 reading_input = False
             elif reading_question_input:
                 # reading from questions section in file.
@@ -155,6 +173,7 @@ def get_configurations():
                             question_variables[2]
                             ))
                         question_variables = []
+                        key += 1  # new unic key for next question
                     elif len(question_variables) == 4:
                         # make an other question
                         questions.append(Question(
@@ -162,13 +181,13 @@ def get_configurations():
                             question_variables[0],
                             question_variables[1],
                             question_variables[2],
-                            question_variables[3]
+                            question_variables[3].strip(" ").strip("(").strip(")").strip(" ").split(",")
                             ))
                         question_variables = []
-                    key += 1  # new unic key for next question
-    return (configurations, questions)
+                        key += 1  # new unic key for next question
+    return (configuration, questions)
 
-def create_datasheet(images, dict_image_id, evaluator_name, datasheet_name=None):
+def create_datasheet(picture_class_seq, dict_image_id, evaluator_name, datasheet_name=None):
     # make a data sheet in the current folder
     if not datasheet_name:
         # create a name for datasheet if none given
@@ -176,16 +195,23 @@ def create_datasheet(images, dict_image_id, evaluator_name, datasheet_name=None)
         datasheet_name = "plaque_identification_of_brain_tissues_" + time.strftime("%Y-%m-%d_%H-%M-%S") + ".csv"
     with open(datasheet_name, 'w', newline='') as datasheet:
         sheet = csv.writer(datasheet, dialect='unix')
-        sheet.writerow(["image_name", "identification", "date", "name_evaluator"])
-        sheet.writerow([images[0], dict_image_id[images[0]], time.strftime("%Y-%m-%d_%H-%M-%S"), evaluator_name])
-        for image in images[1:]:
-            sheet.writerow([image, dict_image_id[image]])
+        respons_category = [qest.name_in_database for qest in st.session_state.questions_to_ask]
+        sheet.writerow(["image_name"] + respons_category + ["date", "name_evaluator"])
+        sheet.writerow([picture_class_seq[0].full_names, dict_image_id[picture_class_seq[0].full_names], time.strftime("%Y-%m-%d_%H-%M-%S"), evaluator_name])
+        for picture in picture_class[1:]:
+            sheet.writerow([picture.full_names, dict_image_id[picture.full_names]])
 
 
 ### --- initialization of session --- ###
 
 if 'image_to_id_dictionary' not in st.session_state:
     st.session_state.image_to_id_dictionary = dict()
+
+if 'configurations' not in st.session_state:
+    st.session_state.configurations = get_configurations()[0]
+
+if 'questions_to_ask' not in st.session_state:
+    st.session_state.questions_to_ask = get_configurations()[1] 
 
 all_pictures = get_all_picture()
 
@@ -199,22 +225,17 @@ if 'current_picture' not in st.session_state:
 if 'keep_identifying' not in st.session_state:
     st.session_state.keep_identifying = True
 
-options = ["plaque", "could be a plaque", "not a plaque", "pass"]
-
 
 ### --- Web page --- ###
 
 st.title("Identify the Brain tissue")
 
 if st.session_state.keep_identifying:
+    responses = []
     with st.sidebar:
-        chosen_id = st.radio(
-            "Identification options:",
-            options)
-        if chosen_id == "pass":
-            st.write("skip this image?")
-        else:
-            st.write(f"identified brain tissue image as {chosen_id}")
+        for i, question in enumerate(st.session_state.questions_to_ask):
+            responses.append(question.ask())
+            st.write(f"identified brain tissue image as {responses[i]}")
 
         picture_slider = st.slider(
             "image size (scale)",
@@ -224,7 +245,8 @@ if st.session_state.keep_identifying:
         next_picture_button = st.button("Next image")
         if next_picture_button:
             # couple responce (<chosen_id>) to the current_picture
-            st.session_state.image_to_id_dictionary[st.session_state.current_picture] = chosen_id
+            st.session_state.image_to_id_dictionary[
+                st.session_state.current_picture.full_names] = responses
             # change scope to new picture or go to #finished with identification
             if st.session_state.picture_seq != []:
                 st.session_state.current_picture = st.session_state.picture_seq.pop()
@@ -234,25 +256,34 @@ if st.session_state.keep_identifying:
                 st.experimental_rerun()
 
     with st.container():
-        # show current picture to identify
-        original_picture, resizeable_picture = st.columns(2)
-        variation = st.session_state.current_picture.types
-
-        with original_picture:
-            st.write("original image")
-            for var in variation:
-                st.write(var)
-                st.session_state.current_picture.show(var, 1)
-
-        with resizeable_picture:
-            st.write(f"image scaled x{picture_slider} times")
-            for var in variation:
-                st.write(var)
-                st.session_state.current_picture.show(var, picture_slider)
-        ''
-        ''
         st.write(f"(image {st.session_state.progress} of {st.session_state.number_of_pictures} images)")
-
+        ''
+        ''
+        # show current image(s) to identify
+        variation = st.session_state.current_picture.types
+        
+        # 
+        columns_standart_images = st.columns(len(variation))
+        for i, col in enumerate(columns_standart_images):
+            with col:
+                st.write(variation[i])
+                st.session_state.current_picture.standard_show(
+                    variation[i],
+                    st.session_state.configurations["Default_scale"]
+                    )
+        
+        if st.session_state.configurations["Rescaleability"] == "Enable":
+            if picture_slider == 1:
+                st.write("original size")
+            else:
+                st.write(f"image scaled x{picture_slider} times")
+            for var in variation:
+                st.write(var)
+                st.session_state.current_picture.show(
+                    var,
+                    picture_slider
+                    )
+        
 else:
     # finished with identification #
     st.subheader("All Brain tissues have been identified.")
@@ -268,6 +299,12 @@ else:
 ''
 'Only for development'
 'Internal values:'
+''
+'questions'
+st.write(st.session_state.questions_to_ask[0].options)
+''
+'configurations'
+st.write(st.session_state.configurations)
 ''
 'picture_seq:'
 st.write(str(st.session_state.picture_seq))
